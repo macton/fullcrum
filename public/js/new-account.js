146e1853de66cc2cd42fd78b4c996a3979df5f52
Mike Acton <macton@gmail.com>
2013-09-25 08:08:49 +0000

    moved from html templates (ejs) to angular on client

--- public/js/new-account.js
+++
@@ -1,9 +0,0 @@
-$( function() {
-  $('#account-connect').on('click', function() {
-    var accountCode = $('#account-code').val().trim();
-    Q.when($.post( '/account-connect', { accountCode: accountCode } ))
-      .then( function() {
-        window.location.replace('/');
-      }).done();
-  });
-});
