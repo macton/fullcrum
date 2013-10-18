7a7c449f10dbb3c84cc4b7f0ade2ad6df6069217
Mike Acton <macton@gmail.com>
2013-10-18 07:55:11 +0000

    do not submit pushSomethingText

--- private/admin/bugSuggestion/bugSuggestion.js
+++ private/admin/bugSuggestion/bugSuggestion.js
@@ -5,12 +5,25 @@ app.controller('bugSuggestion', function($scope) {
   $scope.isSendingBug        = false;
   $scope.isSendingSuggestion = false;
 
-  var thankYouText      = 'Thank you for your feedback! We\'ll definitely take a look at this.';
-  var putSomethingText  = 'Put your bug or suggestion text here.';
-  var thankYouAgainText = 'Don\'t worry. We\'ll read it for sure!';
+  var thankYouText        = 'Thank you for your feedback! We\'ll definitely take a look at this.';
+  var putSomethingText    = 'Put your bug or suggestion text here.';
+  var altPutSomethingText = 'Go ahead and put what\'s on your mind here. We\'re paying attention! Or email us at welovedevs@fullcrum.co';
+  var thankYouAgainText   = 'Don\'t worry. We\'ll read it for sure!';
 
   var sendTodo = function( url ) {
     if ( $scope.commentText.trim() == '' ) {
+      $scope.safeApply( function() {
+        $scope.commentText  = putSomethingText;
+      });
+      return;
+    }
+    if ( $scope.commentText.trim() == putSomethingText ) {
+      $scope.safeApply( function() {
+        $scope.commentText  = altPutSomethingText;
+      });
+      return;
+    }
+    if ( $scope.commentText.trim() == altPutSomethingText ) {
       $scope.safeApply( function() {
         $scope.commentText  = putSomethingText;
       });
