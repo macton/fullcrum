02f5e37e82742bab335a1572ed40e52cb7fa1fb1
Mike Acton <macton@gmail.com>
2013-11-20 06:08:47 +0000

    todo

--- fullcrum.scratch.js
+++ fullcrum.scratch.js
@@ -1,16 +1,27 @@
 
-var fullcrum = require('./fullcrum.api');
-var Qx       = require('qx');
+var fullcrum   = require('./fullcrum.api');
+var Qx         = require('qx');
+var companyId  = '527d14bb9f86ac03db000000'; // filamentgames.com
+var questionnaireInstanceId = '5285024e587a980000000091';
 
 fullcrum.db.connection.then( function() {
-  return fullcrum.db.collection('sessions');
+  // return fullcrum.db.collection( 'Employees' );
+  // return fullcrum.db.collection( 'QuestionnaireInstances' );
+  return fullcrum.db.collection('EmployeeQuestionnaireStatus')
 })
 .then( function( collection ) {
-  return fullcrum.db.collectionFind( collection );
+  var results = { state: 'kUnopened', email: 'norton@filamentgames.com', name: 'Dan Norton', questionnaireInstanceId: questionnaireInstanceId, companyId: companyId };
+  // return fullcrum.db.collectionFindOne( collection, { email: 'norton@filamentgames.com', companyId: companyId } );
+  // return fullcrum.db.collectionFindOne( collection, { companyId: companyId } );
+  return fullcrum.db.collectionInsert( collection, results );
 })
-.then( Qx.map( function( result ) {
-  console.log( result.text );
-}))
+.then( function( result ) {
+  console.log( result );
+})
+.fail( function( err ) {
+  console.log( err.stack );
+})
 .done( function() {
   fullcrum.db.close();
 });
+
